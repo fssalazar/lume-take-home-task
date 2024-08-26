78314c6053ce5e45ff0a99b4d08635cf804ea1fb
fssalazar <salazarfelipe.fs@gmail.com>
2024-08-26 17:17:41 -0300

    first commit

--- src/http/fetchDataset.ts
+++ src/http/fetchDataset.ts
@@ -1,12 +1,14 @@
-import { Person } from "@/entities/person";
+import { Person } from '@/entities/person'
 
 export async function fetchDataset(): Promise<Person[]> {
   try {
-    const response = await fetch('http://localhost:3000/dataset.json', {method: 'GET'})
-    const data = await response.json();
-    return data;
+    const response = await fetch('http://localhost:3000/dataset.json', {
+      method: 'GET',
+    })
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error('Fetch error:', error);
-    throw error;
+    console.error('Fetch error:', error)
+    throw error
   }
 }
